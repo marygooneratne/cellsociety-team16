ecfac06cc0e58213e78b7da6a08bc00e960e1fe8
Hyunjae Lee <hl285@duke.edu>
2019-02-01 17:40:41 -0500

    XML file creator more code

--- src/Configuration/XMLmaker.java
+++ src/Configuration/XMLmaker.java
@@ -13,24 +13,44 @@ import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 public class XMLmaker {
-    public static void main(String argv[]) {
+    public FileMaker(String simType, String simName, String author, int width) {
         try{
             DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
             Document doc = docBuilder.newDocument();
             //Elements for Simulation type and name, author
             Element Simulationtype = doc.createElement("Simulationtype");
-            Simulationtype.appendChild(doc.createTextNode("Fire"));
+            Simulationtype.appendChild(doc.createTextNode(simType));
             doc.appendChild(Simulationtype);
             Element Simtitle = doc.createElement("Simulationtitle");
-            Simtitle.appendChild(doc.createTextNode("Spreading Fire"));
+            Simtitle.appendChild(doc.createTextNode(simName));
             Simulationtype.appendChild(Simtitle);
             Element Author = doc.createElement("Author");
-            Author.appendChild("Hyunjae Lee");
+            Author.appendChild(doc.createTextNode(author));
             Simtitle.appendChild(Author);
-            
+
+            //Elements for Grid and cell initial states
+            Element Width = doc.createElement("Width");
+            Width.appendChild(doc.createTextNode("%d",width))
+            doc.appendChild(Width);
+
+
+
+
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(new File("C:\\file.xml"));
+
+            transformer.transform(source,result);
             //Elements for
 
+        }
+        catch(ParserConfigurationException pce) {
+            pce.printStackTrace();
+        }
+        catch(TransformerException tfe){
+            tfe.printStackTrace();
         }
     }
 }
