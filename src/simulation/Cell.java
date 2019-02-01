18ba533ebc432ec0e0edce452c044836391af348
Mary Gooneratne <mmg53@duke.edu>
2019-02-01 16:49:54 -0500

    Added abstract Cell class

--- src/simulation/Cell.java
+++ src/simulation/Cell.java
@@ -1,4 +1,49 @@
 package simulation;
 
-public class Cell(){
-        }
+public abstract class Cell(){
+    private CellState currentState;
+    private CellState nextState;
+    private ArrayList<Cell> currentNeighbors;
+    private int row;
+    private int column;
+
+    public Cell(){
+
+    }
+
+    public void setCurrentState(CellState newState){
+        this.currentState = newState;
+    }
+
+    public void setNextState(CellState newState){
+        this.nextState = newState;
+    }
+
+    public void setCurrentNeighbors(ArrayList<Cell> newNeighbors){
+        this.currentNeighbors = newNeighbors;
+    }
+
+    public void setRow(int newRow){
+        this.row = newRow;
+    }
+
+    public void setColumn(int newColumn){
+        this.column = newColumn;
+    }
+
+    public CellState getCurrentState(){
+        return this.currentState;
+    }
+
+    public ArrayList<Cell> getCurrentNeighbors(){
+        return this.currentNeighbors;
+    }
+
+    public int getRow(){
+        return this.row;
+    }
+
+    public int getColumn(){
+        return this.column;
+    }
+}
--- src/simulation/READ_ME.md
+++
@@ -1,1 +0,0 @@
-Testing GitLab merge request
