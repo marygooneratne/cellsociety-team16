8ba2e287e8ec1f6f1b415f7b50044fb806149686
AMANDA MADDEN <amm172@duke.edu>
2019-02-04 19:06:19 -0500

    BuildOptions class

---
+++ src/Visualization/BuildOptions.java
@@ -0,0 +1,82 @@
+package Visualization;
+
+import javafx.animation.Timeline;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.LongProperty;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.text.Text;
+
+/** set up the bottom portion of the UI that includes pause, play, speed, and cycle interval */
+
+public class BuildOptions {
+    private static int MIN_FRAMES = 20;
+    private static int MAX_FRAMES = 100;
+    private static int TICK_MARKS = 20;
+    private static String PAUSE_LABEL = "pause";
+    private static String PLAY_LABEL = "play";
+    private static String SPEED_LABEL = "speed: frames/sec";
+    private static String CYCLE_LABEL = "CYCLE: ";
+    private static Group myRoot;
+
+    public BuildOptions(Group optionsRoot, /**Timeline anim,*/ double framesPerS, LongProperty cycleNum){
+        myRoot = optionsRoot;
+        //make buttons
+        Button pause = new Button(PAUSE_LABEL);
+        Button play = new Button(PLAY_LABEL);
+
+        pause.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent ae) {
+//                anim.pause();
+            }
+        });
+
+        play.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent ae) {
+//                anim.play();
+            }
+        });
+
+        //make slider with label
+        Slider speed = new Slider(MIN_FRAMES, MAX_FRAMES, framesPerS);
+        speed.setMajorTickUnit(TICK_MARKS);
+        speed.setShowTickMarks(true);
+        speed.setShowTickLabels(true);
+        Label sliderLabel = new Label(SPEED_LABEL);
+        sliderLabel.setLayoutX(200);
+        sliderLabel.setLayoutY(75);
+        myRoot.getChildren().add(sliderLabel);
+        sliderLabel.setLabelFor(speed);
+
+        //something for slider
+
+        Text cycle = new Text();
+        cycle.textProperty().bind(Bindings.createStringBinding(() -> (CYCLE_LABEL + cycleNum.get())));
+
+        //add to optionsRoot to get to window
+        myRoot.getChildren().add(pause);
+        myRoot.getChildren().add(play);
+        myRoot.getChildren().add(speed);
+        myRoot.getChildren().add(cycle);
+
+        //define layout
+        pause.setLayoutX(0);
+        pause.setLayoutY(0);
+        play.setLayoutX(0);
+        play.setLayoutY(100);
+        speed.setLayoutX(200);
+        speed.setLayoutY(100);
+        cycle.setX(200);
+        cycle.setY(0);
+    }
+
+    public Group getRoot(){
+        return myRoot;
+    }
+}
--- src/Visualization/SceneBuilder.java
+++ src/Visualization/SceneBuilder.java
@@ -4,21 +4,15 @@ import Visualization.GameOfLife.PopulatedCell;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.beans.binding.Bindings;
 import javafx.beans.property.LongProperty;
 import javafx.beans.property.SimpleLongProperty;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.Slider;
-import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Shape;
 import javafx.stage.Stage;
-import javafx.scene.text.Text;
 import javafx.util.Duration;
 
 //import javafx.animation.KeyFrame;
@@ -31,24 +25,18 @@ public class SceneBuilder extends Application {
     private static int GRID_DIM = 400;
     private static int UPDATE_HEIGHT = HEIGHT - GRID_DIM;
     private static Paint BACKGROUND = Color.LIGHTSLATEGRAY;
-    private static int MIN_FRAMES = 20;
-    private static int MAX_FRAMES = 100;
-    private static int TICK_MARKS = 20;
 
     //things that will be read in
     private int myGridSize = 20;
     private int myFramesPerS = 60; // --> as this increases, the sim runs faster
     private int myDelay = 1/myFramesPerS; // seconds --> *1000 for ms
     private static String TITLE = "Cellular Automata";
-    private static String PAUSE_LABEL = "pause";
-    private static String PLAY_LABEL = "play";
-    private static String SPEED_LABEL = "speed: frames/sec";
-    private static String CYCLE_LABEL = "CYCLE: ";
 
     //important for our simulation
     private Scene myScene;
     private Shape[][] myGrid = new Shape[GRID_DIM][GRID_DIM];
     private LongProperty myCycle = new SimpleLongProperty(0);
+    private Timeline myAnimation;
 
     @Override
     public void start(Stage stage) {
@@ -59,10 +47,10 @@ public class SceneBuilder extends Application {
 
         //add animation for simulation loop timeline
         var frame = new KeyFrame(Duration.millis(myDelay*1000), e -> step(myDelay, myGrid));
-        var animation = new Timeline();
-        animation.setCycleCount(Timeline.INDEFINITE);
-        animation.getKeyFrames().add(frame);
-        animation.play();
+        var myAnimation = new Timeline();
+        myAnimation.setCycleCount(Timeline.INDEFINITE);
+        myAnimation.getKeyFrames().add(frame);
+        myAnimation.play();
     }
 
     /** initial simulation with grid and buttons*/
@@ -77,48 +65,13 @@ public class SceneBuilder extends Application {
         Group grid = makeGrid(cellSize);
         window.setTop(grid);
 
-        // updates segment of the window
-        Group options = makeOptions(new Group());
+        // options region of the UI
+        Group options = new BuildOptions(new Group(), /**myAnimation,*/ myFramesPerS, myCycle).getRoot();
 
         window.setBottom(options);
 
         Scene scn = new Scene(window, width, height, bg);
         return scn;
-    }
-
-
-/** method to set up the bottom portion of the UI that includes pause */
-    private Group makeOptions(Group root){
-        Button pause = new Button(PAUSE_LABEL);
-        Button play = new Button(PLAY_LABEL);
-        Slider speed = new Slider(MIN_FRAMES, MAX_FRAMES, myFramesPerS);
-
-        speed.setMajorTickUnit(TICK_MARKS);
-        speed.setShowTickMarks(true);
-        speed.setShowTickLabels(true);
-        Label sliderLabel = new Label(SPEED_LABEL);
-        sliderLabel.setLayoutX(200);
-        sliderLabel.setLayoutY(75);
-        root.getChildren().add(sliderLabel);
-        sliderLabel.setLabelFor(speed);
-
-        Text cycle = new Text();
-        cycle.textProperty().bind(Bindings.createStringBinding(() -> (CYCLE_LABEL + myCycle.get()), myCycle));
-
-        root.getChildren().add(pause);
-        root.getChildren().add(play);
-        root.getChildren().add(speed);
-        root.getChildren().add(cycle);
-
-        pause.setLayoutX(0);
-        pause.setLayoutY(0);
-        play.setLayoutX(0);
-        play.setLayoutY(100);
-        speed.setLayoutX(200);
-        speed.setLayoutY(100);
-        cycle.setX(200);
-        cycle.setY(0);
-        return root;
     }
 
     /**Construct the myGridSize x myGridSize grid for the cells to inhabit*/
@@ -150,6 +103,8 @@ public class SceneBuilder extends Application {
                 }
             }
         }
+
+        myCycle.set(myCycle.get() + 1);
     }
 
     /**
