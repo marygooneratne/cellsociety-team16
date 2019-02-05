1a93c667d9fc8f6f0f9a9e806e68d491d987597e
AMANDA MADDEN <amm172@duke.edu>
2019-02-04 23:36:04 -0500

    cycle number updates

--- src/Visualization/BuildOptions.java
+++ src/Visualization/BuildOptions.java
@@ -1,6 +1,7 @@
 package Visualization;
 
 import javafx.animation.Timeline;
+import javafx.beans.Observable;
 import javafx.beans.binding.Bindings;
 import javafx.beans.property.LongProperty;
 import javafx.event.ActionEvent;
@@ -9,6 +10,7 @@ import javafx.scene.Group;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
 /** set up the bottom portion of the UI that includes pause, play, speed, and cycle interval */
@@ -20,10 +22,9 @@ public class BuildOptions {
     private static String PAUSE_LABEL = "pause";
     private static String PLAY_LABEL = "play";
     private static String SPEED_LABEL = "speed: frames/sec";
-    private static String CYCLE_LABEL = "CYCLE: ";
     private static Group myRoot;
 
-    public BuildOptions(Group optionsRoot, /**Timeline anim,*/ double framesPerS, LongProperty cycleNum){
+    public BuildOptions(Group optionsRoot, Timeline anim, double framesPerS){
         myRoot = optionsRoot;
         //make buttons
         Button pause = new Button(PAUSE_LABEL);
@@ -32,14 +33,14 @@ public class BuildOptions {
         pause.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent ae) {
-//                anim.pause();
+                anim.pause();
             }
         });
 
         play.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent ae) {
-//                anim.play();
+                anim.play();
             }
         });
 
@@ -54,16 +55,18 @@ public class BuildOptions {
         myRoot.getChildren().add(sliderLabel);
         sliderLabel.setLabelFor(speed);
 
-        //something for slider
+//        //something for slider
+//        speed.setBlockIncrement(1);
+//        speed.valueProperty().addListener((Observable, oldValue, newValue) ->{
+//            int i = newValue.intValue();
+//        });
 
-        Text cycle = new Text();
-        cycle.textProperty().bind(Bindings.createStringBinding(() -> (CYCLE_LABEL + cycleNum.get())));
+//        TextField fileInput = new TextField("Please write a valid filename");
 
         //add to optionsRoot to get to window
         myRoot.getChildren().add(pause);
         myRoot.getChildren().add(play);
         myRoot.getChildren().add(speed);
-        myRoot.getChildren().add(cycle);
 
         //define layout
         pause.setLayoutX(0);
@@ -72,8 +75,6 @@ public class BuildOptions {
         play.setLayoutY(100);
         speed.setLayoutX(200);
         speed.setLayoutY(100);
-        cycle.setX(200);
-        cycle.setY(0);
     }
 
     public Group getRoot(){
